3a4bdad65fc6d9d350bb9280e49f3509a5736342
kvalls <kvalls.contact@gmail.com>
2022-12-16 20:31:01 +0000

    Added Header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HeaderComponent } from './components/header/header.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,11 +9,13 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './shared/auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AuthModule],
   providers: [
--- frontend/src/app/shared/services/session.service.ts
+++ frontend/src/app/shared/services/session.service.ts
@@ -7,7 +7,7 @@ import { Storage } from '@ionic/storage';
 })
 export class SessionService {
 
-  AUTH_SERVER_ADDRESS:  string  =  'http://localhost:4000';
+  AUTH_SERVER_ADDRESS:  string  =  'http://'+ window.location.hostname+ ':4000';
 
   constructor(private  httpClient:  HttpClient, private  storage:  Storage) { }
 
