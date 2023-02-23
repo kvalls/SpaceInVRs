704123c66861952d2519b711d8146cb8cb6693f9
kvalls <kvalls.contact@gmail.com>
2023-02-23 20:44:05 +0000

    Updated Jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,31 @@
 // jest.config.js (javascript file)
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts"
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "jestGlobalMocks.ts",
+    ".module.ts",
+    "<rootDir>/src/app/main.ts",
+    ".mock.ts",
+    "coverage",
+    "environments",
+    "main",
+    "polyfills",
+    "zone-flags"
+  ],
+  coverageThreshold: {
+    "global": {
+      "branches": 20,
+      "functions": 20,
+      "lines": 20,
+      "statements": 20
+    }
+  },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup'
